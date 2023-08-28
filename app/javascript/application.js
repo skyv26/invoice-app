64c818b08ff3718e0370a66bb043daa342b10a61
Aakash Verma <dev.aakashv@gmail.com>
2023-08-28 22:17:18 +0530

    `lint`: resolve the linter errors

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,11 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails';
+import 'controllers';
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />,
+  <App />,
 );
