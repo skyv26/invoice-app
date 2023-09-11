7f40701605bb79c5e62bb262f9c6ba5920687239
Aakash Verma <dev.aakashv@gmail.com>
2023-09-11 13:10:02 +0530

    `feat`: generate model for the `user` table

---
+++ db/migrate/20231228073953_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[7.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email_address
+      t.string :contact
+      t.string :date_of_birth
+      t.string :profile_picture
+      t.references :address, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
