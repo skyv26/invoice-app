8396d7c94ac061a9dc325bcadd9d804f5ccaa5b7
Aakash Verma <dev.aakashv@gmail.com>
2023-09-20 14:07:18 +0530

    `feat`: update `columns` with not null property for `items` migration

--- db/migrate/20231228074044_create_items.rb
+++ db/migrate/20231228074044_create_items.rb
@@ -1,9 +1,9 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-      t.string :name
-      t.integer :quantity
-      t.decimal :price_per_unit
+      t.string :name, null: false
+      t.integer :quantity, null: false
+      t.decimal :price_per_unit, null: false, precision: 7, scale: 2
       t.references :invoice, null: false, foreign_key: true
 
       t.timestamps
