ea79eab7f011d156ac1aab55e8dc60831977c0bf
Aakash Verma <dev.aakashv@gmail.com>
2023-09-20 13:58:52 +0530

    `feat`: update `columns` with not null property for `client` migration

--- db/migrate/20231228073926_create_clients.rb
+++ db/migrate/20231228073926_create_clients.rb
@@ -1,8 +1,8 @@
 class CreateClients < ActiveRecord::Migration[7.1]
   def change
     create_table :clients do |t|
-      t.string :name
-      t.string :email_address
+      t.string :name, null: false
+      t.string :email_address, null: false
       t.references :address, null: false, foreign_key: true
 
       t.timestamps
