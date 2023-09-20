c402f275a899815ddcd9a00c933fbda8207cdf23
Aakash Verma <dev.aakashv@gmail.com>
2023-09-20 14:03:54 +0530

    `feat`: update `columns` with not null property for `users` migration

--- db/migrate/20231228073953_create_users.rb
+++ db/migrate/20231228073953_create_users.rb
@@ -1,11 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :first_name
-      t.string :last_name
-      t.string :email_address
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :email_address, null: false
       t.string :contact
-      t.string :date_of_birth
+      t.string :date_of_birth, null: false
       t.string :profile_picture
       t.references :address, null: false, foreign_key: true
 
