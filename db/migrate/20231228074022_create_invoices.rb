edf94179bcbfccee12045f99b9d0c63d1465add1
Aakash Verma <dev.aakashv@gmail.com>
2023-09-20 13:57:32 +0530

    `feat`: update `columns` with not null property

--- db/migrate/20231228074022_create_invoices.rb
+++ db/migrate/20231228074022_create_invoices.rb
@@ -1,11 +1,11 @@
 class CreateInvoices < ActiveRecord::Migration[7.1]
   def change
     create_table :invoices do |t|
-      t.string :description
-      t.integer :payment_terms
-      t.date :payment_due_date
-      t.string :status
-      t.decimal :total
+      t.string :description, null: false
+      t.integer :payment_terms, null: false
+      t.date :payment_due_date, null: false
+      t.string :status, default: 'pending'
+      t.decimal :total, null: false
       t.references :client, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
 
