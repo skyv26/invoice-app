8a3468d5d3faab0e33295229f0eaf7cdcd27df32
Aakash Verma <dev.aakashv@gmail.com>
2023-10-15 15:14:36 +0530

    `feat`: add the `validation` for the `item` model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :invoice
+
+  validates :name, presence: true
+  validates :quantity, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :price_per_unit, presence: true, numericality: { greater_than_or_equal_to: 0, float: true }
 end
