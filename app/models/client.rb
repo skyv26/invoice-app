a0562e64a5e596a32f85579f841377ee6bcf441d
Aakash Verma <dev.aakashv@gmail.com>
2023-10-15 15:04:50 +0530

    `feat`: add the `validation` for the `client` model

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,7 @@
 class Client < ApplicationRecord
   belongs_to :address
   has_one :invoice
+
+  validates :name, presence: true
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
