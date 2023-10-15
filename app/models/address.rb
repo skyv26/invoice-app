59ec7f8c6448a0787f43c58fa6ecdaf5dc5f268c
Aakash Verma <dev.aakashv@gmail.com>
2023-10-15 15:02:43 +0530

    `feat`: add the `validation` for the address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,4 +1,16 @@
 class Address < ApplicationRecord
     has_one :client
     has_one :user
+
+    validate :valid_type
+
+    validates :street, presence: true
+    validates :city, presence: true
+    validates :postal_code, presence: true, length: { minimum: 5, maximum: 10 }
+    validates :country, presence: true
+
+    def valid_type
+        allowed_types = ['client', 'sender']
+        errors.add(:type, 'must be either "client" or "sender"') unless allowed_types.include?(type)
+    end
 end
