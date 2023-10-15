e650ddfaec3079a48cb134f4c0bb781e671e1f15
Aakash Verma <dev.aakashv@gmail.com>
2023-10-15 15:06:22 +0530

    `feat`: add the `validation` for the `user` model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
   belongs_to :address
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email_address, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :date_of_birth, presence: true
 end
