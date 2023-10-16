51dad684900b292baab4dda1ac5fdd7947ecaaba
Aakash Verma <dev.aakashv@gmail.com>
2023-10-16 15:50:13 +0530

    `update`: address model validation with `custom` validation methods for accurate testing

--- app/models/address.rb
+++ app/models/address.rb
@@ -6,13 +6,29 @@ class Address < ApplicationRecord
 
     validate :valid_type
 
-    validates :street, presence: true
-    validates :city, presence: true
-    validates :postal_code, presence: true, length: { minimum: 5, maximum: 10 }
-    validates :country, presence: true
+    validate :street_must_be_string
+    validate :city_must_be_string
+    validate :postal_code_must_be_string
+    validate :country_must_be_string
 
     def valid_type
         allowed_types = ['client', 'sender']
         errors.add(:type, 'must be either "client" or "sender"') unless allowed_types.include?(type)
+    end
+
+    def street_must_be_string
+        errors.add(:street, 'must be a string') unless street_before_type_cast.is_a?(String)
+    end
+
+    def city_must_be_string
+        errors.add(:city, 'must be a string') unless city_before_type_cast.is_a?(String)
+    end
+
+    def postal_code_must_be_string
+        errors.add(:postal_code, 'must be a string and between 5-10 digits') unless postal_code_before_type_cast.is_a?(String)
+    end
+
+    def country_code_must_be_string
+        errors.add(:country, 'must be a string') unless country_code_before_type_cast.is_a?(String)
     end
 end
