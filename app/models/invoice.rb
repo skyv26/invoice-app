a9071d8d4ed5964c7d21f3a7fe2c180f8e1e8d31
Aakash Verma <dev.aakashv@gmail.com>
2023-10-29 17:51:39 +0530

    `update`: invoice model validation with `custom` validation method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -3,7 +3,7 @@ class Invoice < ApplicationRecord
   belongs_to :user
   has_many :items
 
-  validates :description, presence: true
+  validate :description_must_be_string
   validates :payment_terms, presence: true, numericality: { only_integer: true, greater_than: 0 }
   validates :payment_due_date, presence: true
   validate :valid_status
@@ -12,6 +12,18 @@ class Invoice < ApplicationRecord
   def valid_status
     allowed_status = ['pending', 'paid', 'default']
     errors.add(:status, 'must be either "client" or "sender" or "default"') unless allowed_status.include?(status)
+  end
+
+  def description_must_be_string
+    description_before = description_before_type_cast
+    
+    errors.add(:description, 'must be a string') unless description_before.is_a?(String)
+
+    return unless description_before.is_a?(String)
+    
+    if description_before.size <= 10 || description_before.size >= 50 
+        errors.add(:description, 'should be valid and between 10-50 characters long')
+    end
   end
 
 end
