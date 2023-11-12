d65a93c3d28a6ab7bee450b7ac229406d8d265f6
Aakash Verma <dev.aakashv@gmail.com>
2023-11-12 20:14:23 +0530

    `update`: `routes` by adding the `Api::V1` namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+    end
+  end
   get 'roots/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
