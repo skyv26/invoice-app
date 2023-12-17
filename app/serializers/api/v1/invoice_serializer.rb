3e08242614050ef9398b7adb8559f980a4acccad
Aakash Verma <dev.aakashv@gmail.com>
2023-12-17 22:08:08 +0530

    `update`: user serializer by adding the invoices association

--- app/serializers/api/v1/invoice_serializer.rb
+++ app/serializers/api/v1/invoice_serializer.rb
@@ -1,3 +1,5 @@
 class Api::V1::InvoiceSerializer < ActiveModel::Serializer
   attributes :id, :description, :payment_due_date, :payment_terms, :status, :total
+  belongs_to :client, serializer: Api::V1::ClientSerializer
+  has_many :items, serializer: Api::V1::ItemSerializer
 end
